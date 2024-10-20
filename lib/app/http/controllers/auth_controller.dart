b68310fea2de74d4ec1bfa1113b42685eae5776c
afifShyam <afifaxelo12@gmail.com>
2024-10-21 01:26:18 +0800

    complete: user login and register for controller

--- lib/app/http/controllers/auth_controller.dart
+++ lib/app/http/controllers/auth_controller.dart
@@ -1,49 +1,125 @@
-import 'package:backend_vania/services/index.dart';
+// ignore: implementation_imports
+import 'package:vania/src/exception/validation_exception.dart';
+
+import 'dart:io';
 import 'package:vania/vania.dart';
+import 'package:backend_vania/app/models/user.dart';
 
 class AuthController extends Controller {
-  AuthService authService = AuthService();
-
   Future<Response> register(Request request) async {
-    final username = request.body['username'];
-    final password = request.body['password'];
-    final email = request.body['email'];
+    try {
+      request.validate({
+        'username': 'required|string',
+        'password': 'required|string',
+        'email': 'required|email'
+      });
+    } catch (e) {
+      if (e is ValidationException) {
+        String errorMessage = e.message;
+        int code = e.code;
+        return Response.json({"message": errorMessage, "result": false}, code);
+      } else {
+        return Response.json(
+            {"message": "unknown error", "result": false}, 520);
+      }
+    }
 
-    request.validate({
-      username: 'required|string',
-      password: 'required|string',
-      email: 'required|email'
-    });
-    final hashPassword = authService.hashPassword(password);
-    request.query().selec();
-    return Response(
-        data: 'User Successful Register',
-        responseType: ResponseType.json,
-        httpStatusCode: 201);
+    try {
+      final username = request.input('username');
+      final hashPassword = Hash().make(request.input('password'));
+      final email = request.input('email');
+
+      final user =
+          await User().query().where('username', '=?', [username]).first();
+
+      if (user != null) {
+        return Response.json(
+          {
+            'message': 'User already exists, please choose another username',
+            'result': false
+          },
+          HttpStatus.badRequest,
+        );
+      }
+
+      await User().query().insert({
+        'username': username,
+        'email': email,
+        'password': hashPassword,
+        'created_at': DateTime.now().toIso8601String(),
+      });
+
+      return Response.json(
+        {'Message': 'User Successful Register'},
+        HttpStatus.ok,
+      );
+    } catch (e) {
+      return Response.json(
+        {"message": "unknown error", "result": false},
+        HttpStatus.badGateway,
+      );
+    }
   }
 
   Future<Response> login(Request req) async {
-    final body = req.body;
-    final username = body['username'];
-    final password = body['password'];
-
-    req.validate(
-      {
-        username: 'required|string',
-        password: 'required|string',
-      },
-    );
-
-    final user =
-        req.query().select(['* users']).where('email', '=?', [username]);
-
-    if (user == null ||
-        !authService.verifyPassword(password, user[0]['password'])) {
-      return Response(httpStatusCode: 401, data: 'Invalid password');
+    try {
+      req.validate(
+        {
+          'username': 'required|string',
+          'password': 'required|string',
+        },
+      );
+    } catch (e) {
+      if (e is ValidationException) {
+        String errorMessage = e.message;
+        int code = e.code;
+        return Response.json({"message": errorMessage, "result": false}, code);
+      } else {
+        return Response.json(
+            {"message": "unknown error", "result": false}, 520);
+      }
     }
 
-    final token = authService.generateJwtToken(user[0]['id']);
-    return Response.json({'token': token});
+    try {
+      final username = req.input('username');
+      final password = req.input('password');
+
+      final user = await User()
+          .query()
+          .select(['*']).where('username', '=?', [username]).first();
+
+      if (user == null) {
+        return Response.json(
+          {'message': 'User not found', 'result': false},
+          HttpStatus.notFound,
+        );
+      }
+
+      if (!Hash().verify(password, user['password'])) {
+        return Response.json(
+          {'message': 'Wrong password', 'result': false},
+          HttpStatus.unauthorized,
+        );
+      } else {
+        final auth = Auth().login(user);
+        final token = await auth.createToken(expiresIn: Duration(days: 62));
+        user.removeWhere((key, value) => key == 'password');
+        return Response.json(
+          {
+            'message': 'Login Successful',
+            'result': true,
+            'token': token['access_token'],
+            'user': user
+          },
+          HttpStatus.ok,
+        );
+      }
+    } catch (e) {
+      return Response.json(
+        {"message": "unknown error", "result": false},
+        HttpStatus.badGateway,
+      );
+    }
   }
 }
 
---
+++ lib/app/models/weather.dart
@@ -0,0 +1,7 @@
+import 'package:vania/vania.dart';
+
+class WeatherModel extends Model {
+  WeatherModel() {
+    super.table('weather_table');
+  }
+}
